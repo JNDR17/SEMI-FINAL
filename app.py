cd2420bf82c1c107e54f5599cee6c82f93915cef
JNDR17 <JNDR17>
2025-05-06 19:50:07 +0800

    added templates

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Dionar, I love you ❤️'
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "This is the about page!"  # You can customize this page later.
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use dynamic port or default to 5000
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
